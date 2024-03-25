4150445ab865746efafbcdcab416346f3eaf3959
Janek1010 <janekdomozych99@gmail.com>
2024-03-25 14:55:13 +0100

    section 10: services done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,20 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://cdn.stocksnap.io/img-thumbs/960w/food-recipe_G8QICMKLUV.jpg'),
-    new Recipe('Another Test Recipe','This is simply a test','https://cdn.stocksnap.io/img-thumbs/960w/food-recipe_G8QICMKLUV.jpg')
-  ];
-  @Output() recipeWasSelected = new EventEmitter <Recipe>();
+export class RecipeListComponent implements  OnInit{
+  recipes: Recipe[];
 
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  constructor(private recipeService: RecipeService) {
   }
+
+  ngOnInit(): void {
+    this.recipes =this.recipeService.getRecipes();
+  }
+
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,37 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe('Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://images.immediate.co.uk/production/volatile/sites/2/2021/10/WienerSchnitzel_015-d281ef4.jpg?quality=90&crop=13px,4395px,5656px,2433px&resize=556,505',
+      [
+        new Ingredient('Meat',1),
+        new Ingredient('French Fries',20)
+      ]
+    ),
+    new Recipe('Nice Pizza',
+      'So delicious',
+      'https://upload.wikimedia.org/wikipedia/commons/9/91/Pizza-3007395.jpg',
+      [
+        new Ingredient('Buns',2),
+        new Ingredient('Meat',1)
+      ]
+    )
+  ];
+  getRecipes(){
+    return this.recipes.slice();
+  }
+  constructor(private shoppingListService: ShoppingListService) { }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients)
+  }
+}
