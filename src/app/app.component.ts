ccfa385ff0442fe93d47856f6ac9b0813d61c3fc
Janek1010 <janekdomozych99@gmail.com>
2024-04-01 22:18:19 +0200

    section 14: Subject added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  // title = 'cookingApp';
-  // loadedFeature = 'recipe';
-  //
-  // onNavigate(feature: string) {
-  //   this.loadedFeature = feature;
-  // }
 }
